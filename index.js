642956d49114c651127aeb937ce6f4456a083418
Angel <loaizadavila.angeldario@gmail.com>
2020-03-10 18:00:47 +0000

    cambios hechos con respecto a la pag.web

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 var app = express();
 var port= process.env.PORT || 80;
-
+app.use("/", express.static("./public"));
 app.get("/time", (request, response) => {
 	response.send(new Date());
 })
-app.listen(port);
+app.listen(port, ()=>{
+	console.log("This server is 100% ready")
+});
 
-console.log("Server ready");
+console.log("The server is not 100% ready, please wait");
