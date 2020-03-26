96324df3be1d913339628b74e84d26f6035a9f32
franbornos6 <fran_bornos6@hotmail.com>
2020-03-26 16:34:10 +0000

    Puesta url de heroku

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get(BASE_PATH + "/electricity-produced-stats/docs/", (req, res) => {
     });
 
 app.get(BASE_PATH + "/motogp-statistics/docs/", (req, res) => {
-        res.redirect("URL POSTMAN");
+        res.redirect("https://documenter.getpostman.com/view/10864755/SzYUa1rg");
     });
 app.get(BASE_PATH + "/ucl_stats/docs/", (req, res) => {
         res.redirect("https://documenter.getpostman.com/view/10690065/SzYUa1nB");
