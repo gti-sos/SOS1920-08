0b864fa0ef90e158ad9a65dccf3e1292a41e0d8f
franbornos6 <fran_bornos6@hotmail.com>
2020-03-26 11:41:00 +0000

    actualizada con mi API

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ app.use(bodyParser.json());
 app.use("/", express.static("./public"));
 var electricityProduced = [];
  
+
+//----------------------------------------------API ANGEL-------------------------------------------------------------------
+
 app.get(BASE_PATH + "/electricity-produced-stats/docs/", (req, res) => {
         res.redirect("https://documenter.getpostman.com/view/10851956/SzYUXfsE");
+    });
+
+app.get(BASE_PATH + "/motogp-statistics/docs/", (req, res) => {
+        res.redirect("URL POSTMAN");
     });
 
 // GET electricityProduced/loadInitialData
@@ -143,6 +150,144 @@ app.put(BASE_PATH + "/electricity-produced-stats/:country/:state", (req, res)=>{
 	}
 	
 });
+
+
+//------------------------------------------------API FRAN---------------------------------------------------
+
+var motogp_statistics = [];
+
+//---------------- /GET motogp-statistics/loadInitialData -------------------------
+
+app.get(BASE_PATH + "/motogp-statistics/loadInitialData", (req, res) =>{
+	
+	var motogp_statisticsInitial = [
+		{
+		country: "Spain",
+		pilot: "Jorge_Lorenzo",
+		last_title: 2015,
+		world_title: 3,
+		victory: 47,
+		podium: 114
+		},
+
+		{
+		country: "Spain",
+		pilot: "Marc_Marquez",
+		last_title: 2019,
+		world_title: 6,
+		victory: 56,
+		podium: 95
+		}
+	];
+	
+	motogp_statistics = motogp_statisticsInitial;
+	res.send(JSON.stringify(motogp_statistics,null,2));
+});
+
+
+//--------------------- GET /motogp-statistics---------------------------------------
+
+app.get(BASE_PATH+"/motogp-statistics", (req,res) =>{
+	
+	if(motogp_statistics == 0){
+		res.status(404).send("No hay datos cargados");
+	}else{
+		res.send(JSON.stringify(motogp_statistics,null,2));	
+	}
+	
+});
+
+//-------------------- POST /motogp-statistics---------------------------------------
+
+app.post(BASE_PATH+"/motogp-statistics", (req,res) =>{
+	
+	var newStat = req.body;
+	
+	if((newStat == "") || (newStat.country == "") || (newStat.name == "")){
+		res.sendStatus(400, "BAD REQUEST");
+	}else{
+		motogp_statistics.push(newStat);
+		res.sendStatus(201,"CREATED");
+	}
+});
+
+// --------------------- PUT /motogp-statistics------------------------------------
+
+app.put(BASE_PATH + "/motogp-statistics", (req,res) => {
+		res.sendStatus(405,"Method Not Allowed");
+});
+
+//--------------------- DELETE /motogp-statistics-----------------------------------
+
+app.delete(BASE_PATH + "/motogp-statistics", (req,res) => {
+	motogp_statistics = [];
+	res.sendStatus(200, "OK");
+});
+
+
+//---------------------- GET /motogp-statistics/:pilot----------------------------------
+
+app.get(BASE_PATH+"/motogp-statistics/:pilot", (req,res)=>{
+	
+	var pilot = req.params.pilot;
+	
+	var filteredMotogpStatstats = motogp_statistics.filter((c) => {
+		return (c.pilot == pilot);
+	});
+	
+	if(filteredMotogpStatstats.length >= 1){
+		res.send(filteredMotogpStatstats[0]);
+	}else{
+		res.sendStatus(404,"NOT FOUND");
+	}
+});
+
+// ---------------------- POST /motogp-statistics/:pilot----------------------------------
+
+	app.post(BASE_PATH + "/motogp-statistics/:pilot", (req,res) => {
+		res.sendStatus(405,"Method NOT Allowed");
+	});
+
+//----------------------- PUT /motogp-statistics/:pilot-----------------------------------
+
+	app.put(BASE_PATH + "/motogp-statistics/:pilot", (req,res) => {
+		
+		var original = req.params.pilot;
+		var modificada = req.body.pilot;
+		
+		if(modificada != original){
+			res.sendStatus(400,"BAD REQUEST");
+			
+		}else{
+			var filteredPilot = motogp_statistics.filter(p => p.pilot != original);
+			motogp_statistics = filteredPilot;
+			motogp_statistics.push(req.body);
+			res.sendStatus(200,"OK");
+		}
+	})
+
+
+
+//----------------------- DELETE /motogp-statistics/:pilot--------------------------------
+
+app.delete(BASE_PATH+"/motogp-statistics/:pilot", (req,res)=>{
+	
+	var pilot = req.params.pilot
+	
+	var filteredMotogpStatstats = motogp_statistics.filter((c) => {
+		return (c.pilot != pilot);
+	});
+	
+	if(filteredMotogpStatstats.length < motogp_statistics.length){
+		motogp_statistics = filteredMotogpStatstats;
+		res.sendStatus(200,"OK");
+	}else{
+		res.sendStatus(404,"NOT FOUND");
+	}
+});
+
+
+//-----------------------------API JOSE CARLOS-------------------------------------------------------------------
 
 app.listen(port, () => {
 	console.log("Server ready");
