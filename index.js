c4e071d25be30058620aaf231beb3e47d8ccba3b
Angel <loaizadavila.angeldario@gmail.com>
2020-03-26 10:53:43 +0000

    probando cosas para el despliegue

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const BASE_PATH = "/api/v1";
 app.use(bodyParser.json());
 app.use("/", express.static("./public"));
 var electricityProduced = [];
+ 
+app.get(BASE_PATH + "/electricity-produced-stats/docs/", (req, res) => {
+        res.redirect("https://documenter.getpostman.com/view/10851956/SzYUXfsE");
+    });
 
 // GET electricityProduced/loadInitialData
 app.get(BASE_PATH + "/electricity-produced-stats/loadInitialData", (req, res)=> {
