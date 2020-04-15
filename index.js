e5c24e94d4be73dbfa2d5f41d9cfe7f2a8605829
Angel <loaizadavila.angeldario@gmail.com>
2020-04-15 21:58:34 +0000

    Datos añadidos Api Angel

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const path = require("path");
 const electricityAPI= require(path.join(__dirname, "electricityAPI"));
 const motogpAPI = require(path.join(__dirname,"motogpAPI"));
 const uclAPI = require(path.join(__dirname,"uclAPI"));
-
-const BASE_API_URL = '/api/v1';
 const BASE_PATH = '/api/v1';
 const port = process.env.PORT || 80;
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,7 +15,7 @@ app.get(BASE_PATH + '/electricity-produced-stats/docs/', (req, res) => {
 	res.redirect('https://documenter.getpostman.com/view/10851956/SzYUXfsE');
 	});
 
-app.get(BASE_API_URL + '/motogp-statistics/docs/', (req, res) => {
+app.get(BASE_PATH + '/motogp-statistics/docs/', (req, res) => {
 	res.redirect('https://documenter.getpostman.com/view/10864755/SzYUa1rg');
 	});
 
