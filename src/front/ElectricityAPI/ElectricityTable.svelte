a5cd48d00aa1e077fc8bea0a95cdfa65aa0a3b96
Angel <loaizadavila.angeldario@gmail.com>
2020-05-07 11:42:56 +0200

    Angel

--- src/front/ElectricityAPI/ElectricityTable.svelte
+++ src/front/ElectricityAPI/ElectricityTable.svelte
@@ -19,7 +19,7 @@
         solar: "",
         coal: ""
     }
-    
+
     onMount(getElegtricityStats);
 
     //pag vars
@@ -41,7 +41,7 @@
 
     async function loadInitialData() {
         const res = await fetch("/api/v1/electricity-produced-stats/loadInitialData", {
-            method : "GET"
+            method: "GET"
         }).then(function (res) {
             getElegtricityStats();
         });
@@ -49,10 +49,10 @@
 
 
     async function getElegtricityStats() {
-       const res = await fetch("/api/v1/electricity-produced-stats");
+        const res = await fetch("/api/v1/electricity-produced-stats");
         if (res.ok) {
             const json = await res.json();
-            
+
             electricity = json;
             console.log("Recieved " + electricity.lenght + "stats");
         } else {
@@ -60,7 +60,7 @@
         }
     }
 
- 
+
     async function insertNewElectricityStat() {
         const res = await fetch("/api/v1/electricity-produced-stats", {
             method: "POST",
@@ -71,7 +71,7 @@
         }).then(function (res) {
             getElegtricityStats();
             if (res.status == 200) {
-              succMsg= res.statusText + " Muestra insertada con exito";
+                succMsg = res.statusText + " Muestra insertada con exito";
                 succMsg = res.status + ":" + res.statusText;
             } else if (res.status == 400) {
                 window.alert("ERROR: Se deben completar todos los campos");
@@ -91,156 +91,158 @@
             method: "DELETE"
         }).then(function (res) {
             getElegtricityStats();
-            succMsg= res.statusText + "Muestra eliminada con exito";
+            succMsg = res.statusText + "Muestra eliminada con exito";
         })
     }
-    async function busqueda(SCountry, SState, SYear, SHydroMin, SHydroMax,SSolarMin, SSolarMax, SCoalMin, SCoalMax) {
-		
-		if(typeof SCountry== 'undefined'){
-             SCountry= "";
+    async function busqueda(SCountry, SState, SYear, SHydroMin, SHydroMax, SSolarMin, SSolarMax, SCoalMin, SCoalMax) {
+
+        if (typeof SCountry == 'undefined') {
+            SCountry = "";
         }
-		if(typeof SState== 'undefined'){
-             SState= "";
+        if (typeof SState == 'undefined') {
+            SState = "";
         }
-        if(typeof SYear== 'undefined'){
-             SYear= "";
+        if (typeof SYear == 'undefined') {
+            SYear = "";
         }
-        if(typeof SHydroMin== 'undefined'){
-             SHydroMin= "";
+        if (typeof SHydroMin == 'undefined') {
+            SHydroMin = "";
         }
-        if(typeof SHydroMax== 'undefined'){
-             SHydroMax= "";
+        if (typeof SHydroMax == 'undefined') {
+            SHydroMax = "";
         }
-         if(typeof SSolarMin== 'undefined'){
-             SSolarMin= "";
+        if (typeof SSolarMin == 'undefined') {
+            SSolarMin = "";
         }
-        if(typeof SSolarMax== 'undefined'){
-             SSolarMax= "";
+        if (typeof SSolarMax == 'undefined') {
+            SSolarMax = "";
         }
-        if(typeof SCoalMin== 'undefined'){
-             SCoalMin= "";
+        if (typeof SCoalMin == 'undefined') {
+            SCoalMin = "";
         }
-        if(typeof SCoalMax== 'undefined'){
-             SCoalMax= "";
+        if (typeof SCoalMax == 'undefined') {
+            SCoalMax = "";
         }
         const res = await fetch("/api/v1/electricity-produced-stats?country=" + SCountry + "&state=" + SState + "&year=" + SYear
-        + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
-        + "&coalMax=" + SCoalMax
+            + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
+            + "&coalMax=" + SCoalMax
         )
-        if(res.ok){
+        if (res.ok) {
             const json = await res.json();
-            electricity= json;
-            console.log( "Found: " + electricity.lenght + " stats");
+            electricity = json;
+            console.log("Found: " + electricity.lenght + " stats");
 
-            if(electricity.lenght == 1){
+            if (electricity.lenght == 1) {
                 succMsg = "Se ha encontrado " + electricity.lenght + " muestra";
-            }else{
+            } else {
                 succMsg = "Se han encontrado " + electricity.lenght + " muestras";
             }
-        }else if(res.status == 400){
+        } else if (res.status == 400) {
             window.alert("No se han encontrado muestras con los parametros introducidos");
         }
-	}
-    
-    async function paginacion(SCountry, SState, SYear, SHydroMin, SHydroMax, SSolarMin, SSolarMax, SCoalMin, SCoalMax, num){
-        if(typeof SCountry== 'undefined'){
-             SCountry= "";
+    }
+
+    async function paginacion(SCountry, SState, SYear, SHydroMin, SHydroMax, SSolarMin, SSolarMax, SCoalMin, SCoalMax, num) {
+        if (typeof SCountry == 'undefined') {
+            SCountry = "";
         }
-		if(typeof SState== 'undefined'){
-             SState= "";
+        if (typeof SState == 'undefined') {
+            SState = "";
         }
-        if(typeof SYear== 'undefined'){
-             SYear= "";
+        if (typeof SYear == 'undefined') {
+            SYear = "";
         }
-        if(typeof SHydroMin== 'undefined'){
-             SHydroMin= "";
+        if (typeof SHydroMin == 'undefined') {
+            SHydroMin = "";
         }
-        if(typeof SHydroMax== 'undefined'){
-             SHydroMax= "";
+        if (typeof SHydroMax == 'undefined') {
+            SHydroMax = "";
         }
-         if(typeof SSolarMin== 'undefined'){
-             SSolarMin= "";
+        if (typeof SSolarMin == 'undefined') {
+            SSolarMin = "";
         }
-        if(typeof SSolarMax== 'undefined'){
-             SSolarMax= "";
+        if (typeof SSolarMax == 'undefined') {
+            SSolarMax = "";
         }
-        if(typeof SCoalMin== 'undefined'){
-             SCoalMin= "";
+        if (typeof SCoalMin == 'undefined') {
+            SCoalMin = "";
         }
-        if(typeof SCoalMax== 'undefined'){
-             SCoalMax= "";
+        if (typeof SCoalMax == 'undefined') {
+            SCoalMax = "";
         }
-        if(num==1){
-            page= page-limit;
-            if(page < 0){
-                page=0;
+        if (num == 1) {
+            page = page - limit;
+            if (page < 0) {
+                page = 0;
                 const res = await fetch("/api/v1/electricity-produced-stats?country=" + SCountry + "&state=" + SState + "&year=" + SYear
-        + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
-        + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
-        )
-        if(res.ok){
-            const json= await res.json();
-            electricity= json;
-        }
-            } else{
+                    + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
+                    + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
+                )
+                if (res.ok) {
+                    const json = await res.json();
+                    electricity = json;
+                }
+            } else {
                 const res = await fetch("/api/v1/electricity-produced-stats?country=" + SCountry + "&state=" + SState + "&year=" + SYear
-        + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
-        + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
-        )
-        if(res.ok){
-            const json= await res.json();
-            electricity= json;
+                    + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
+                    + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
+                )
+                if (res.ok) {
+                    const json = await res.json();
+                    electricity = json;
+                }
+            }
+        } else {
+            page = page + limit;
+            const res = await fetch("/api/v1/electricity-produced-stats?country=" + SCountry + "&state=" + SState + "&year=" + SYear
+                + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
+                + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
+            )
+            if (res.ok) {
+                const json = await res.json();
+                electricity = json;
+            } else if (res.status == 400) {
+                window.alert("No existen mas muestras");
             }
         }
-    }else{
-        page = page + limit;
-        const res = await fetch("/api/v1/electricity-produced-stats?country=" + SCountry + "&state=" + SState + "&year=" + SYear
-        + "&hydroMin= " + SHydroMin + "&hydroMax=" + SHydroMax + "&solarMin=" + SSolarMin + "&solarMax" + SSolarMax + "&coalMin= " + SCoalMin
-        + "&coalMax=" + SCoalMax + "&limit=" + limit + "&offset=" + page
-        )
-        if(res.ok){
-            const json= await res.json();
-            electricity= json;
-    }else if(res.status==400){
-        window.alert("No existen mas muestras");
-    }
-}
     }
 </script>
-<main >
+<main>
     <html lang="ES">
     <meta charset="UTF-8">
-    <body style="background-color: #e6f0ff; font-family: Arial, Helvetica, sans-serif;">
-    <h2 style="text-align: center;">Electricidad Producida</h2>
-    <div style="text-align:left;padding-bottom: 3%;">
-        <Button outline color="primary" on:click={loadInitialData}>Cargar Datos</Button>
-        <Button outline color="danger" on:click={deleteAllStats} onclick="location.reload()">Borrar
-            todos los datos</Button>
-         <Button outline color="secondary" on:click="{pop}">Back</Button>
-    </div>
-   <div style="border: 1px; border-color: black; border-style: groove; padding-bottom: 1%;">
-    <h6>Seccion de busqueda: </h6>
-    <tr>
-		<td><label>Pais: <input bind:value="{SCountry}"></label></td>
-        <td><label>Año: <input bind:value="{SYear}"></label></td>
-        <td><label>Estado/Provincia: <input bind:value="{SState}"></label></td>
-		<td><label>Min Hydro producida: <input bind:value="{SHydroMin}"></label></td>
-        <td><label>Max Hydro producida: <input bind:value="{SHydroMax}"></label></td>
-       
-        
-	</tr>
-	<tr>
-        <td><label>Min E.Carbon Producida: <input bind:value="{SCoalMin}"></label></td>
-        <td><label>Máx E.Carbon producida: <input bind:value="{SCoalMax}"></label></td>
-        <td><label>Min E.Solar Producida: <input bind:value="{SSolarMin}"></label></td>
-        <td><label>Max E.Solar Producida: <input bind:value="{SSolarMax}"></label></td>
-		
-		
-	</tr>
 
-	<Button outline color="primary" on:click="{busqueda (SCountry, SState, SYear, SHydroMin, SHydroMax, SSolarMin, SSolarMax, SCoalMin, SCoalMax)}">Buscar</Button>
-    </div>
-    {#await electricity}
+    <body style="background-color: #e6f0ff; font-family: Arial, Helvetica, sans-serif;">
+        <h2 style="text-align: center;">Electricidad Producida</h2>
+        <div style="text-align:left;padding-bottom: 3%;">
+            <Button outline color="primary" on:click={loadInitialData}>Cargar Datos</Button>
+            <Button outline color="danger" on:click={deleteAllStats} onclick="location.reload()">Borrar
+                todos los datos</Button>
+            <Button outline color="secondary" on:click="{pop}">Back</Button>
+        </div>
+        <div style="border: 1px; border-color: black; border-style: groove; padding-bottom: 1%;">
+            <h6>Seccion de busqueda: </h6>
+            <tr>
+                <td><label>Pais: <input bind:value="{SCountry}"></label></td>
+                <td><label>Año: <input bind:value="{SYear}"></label></td>
+                <td><label>Estado/Provincia: <input bind:value="{SState}"></label></td>
+                <td><label>Min Hydro producida: <input bind:value="{SHydroMin}"></label></td>
+                <td><label>Max Hydro producida: <input bind:value="{SHydroMax}"></label></td>
+
+
+            </tr>
+            <tr>
+                <td><label>Min E.Carbon Producida: <input bind:value="{SCoalMin}"></label></td>
+                <td><label>Máx E.Carbon producida: <input bind:value="{SCoalMax}"></label></td>
+                <td><label>Min E.Solar Producida: <input bind:value="{SSolarMin}"></label></td>
+                <td><label>Max E.Solar Producida: <input bind:value="{SSolarMax}"></label></td>
+
+
+            </tr>
+
+            <Button outline color="primary"
+                on:click="{busqueda (SCountry, SState, SYear, SHydroMin, SHydroMax, SSolarMin, SSolarMax, SCoalMin, SCoalMax)}">Buscar</Button>
+        </div>
+        {#await electricity}
         Loading data...
     {:then electricity}
         <Table  style="border-style: groove; border-width: 1px; padding-block-start: 1%;">
