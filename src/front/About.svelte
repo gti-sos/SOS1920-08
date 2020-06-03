39edd5be0a4b5a5521cae1262150196b2e4aabc1
franbornos6 <fran_bornos6@hotmail.com>
2020-06-03 18:53:16 +0200

    fran

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -1,5 +1,5 @@
 <ul style="list-style-type:circle; font-size: 20px;">
-    <li><a href="">Video API Moto Gp  (Francisco Jurado Garrido)</a></li>
+    <li><a href="">Video API Moto Gp  borrar esto(Francisco Jurado Garrido)</a></li>
     <li><a href="">Video API UCL  (Jose Carlos Molina Coloma)</a></li>
     <li><a href="">Video API Electricity  Angel Loaiza Davila)</a></li>
 </ul>
